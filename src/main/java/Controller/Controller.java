bb399b23b4135d8bce1a67ea09681984c18bfbe9
Pedro Cubal <pedrocubal@gmail.com>
2022-04-08 16:40:40 +0100

    Metodo de subtracao criado

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,4 +1,11 @@
 package Controller;
 
+import model.Numero;
+
 public class Controller {
+
+    public double getFormulaSubtracao(double x, double y){
+        return Numero.formulaSubtracao(x, y);
+    }
+
 }
---
+++ src/main/java/model/Numero.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Numero {
+
+    public static double formulaSubtracao(double x, double y){
+        return (x-y);
+    }
+}
