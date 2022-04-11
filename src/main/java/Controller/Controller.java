7c3d420fe19e85ae85fef61613b37a20e614d583
Sofia Silva <sofiamarques1@gmail.com>
2022-04-11 09:24:48 +0100

    atualização classe numero com adiçao de construtores etc etc e método quociente

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,11 +1,43 @@
-package Controller;
+package controller;
 
-import model.Numero;
+import model.Calculadora;
 
 public class Controller {
 
-    public double getFormulaSubtracao(double x, double y){
-        return Numero.formulaSubtracao(x, y);
-    }
+        public double getFormulaSubtracao(double x, double y){
+            return Calculadora.formulaSubtracao(x, y);
+        }
+
+        public double getFormulaSoma(double x,double y){
+            return  Calculadora.formulaSoma(x,y);
+        }
+
+        public double getFormulaQuociente( double x, double y){
+            return Calculadora.formulaQuociente (x,y);
+
+        }
+
+        public double getFormulaProduto(double x,double y){
+            return Calculadora.formulaProduto(x,y);
+
+        }
+
+        /*public void mostrarResultado(){
+            Calculadora.mostrarResultado();
+        }*/
+
+        /*public  boolean getVerificarNumeroPrimo(){
+            Calculadora.verifarNumeroPrimo();
+            return true;
+        }*/
+
+       // public double getCalculoFatorial() {
+          //  return  Calculadora.calcularFatorial();;
+      //  }
+
+
+       // public double getCalculoXelevadoY(){
+//            return Numero.calcXelevadoY();
+//        }
 
 }
